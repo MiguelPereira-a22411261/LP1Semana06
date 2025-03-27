5e78325619e7f6fc3b51ed1e3f09b89d061bcdb4
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-03-27 11:07:19 +0000

    Add cicle for the inputted argument

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -7,6 +7,14 @@ namespace RandDice
         private static void Main(string[] args)
         {
             Random s = new Random(int.Parse(args[1]));
+            int add = 0;
+
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {
+                add += s.Next(1, 7);
+            }
+            Console.WriteLine(add);
+
         }
     }
 }
