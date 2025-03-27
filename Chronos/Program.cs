36adea0b639aae9aef84baa6b1d7fe8877de5525
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-03-27 10:15:43 +0000

    Create elapsed times for crono 1 and 2

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -13,10 +13,16 @@ namespace Chronos
 
             crono1.Start();
             Thread.Sleep(500);
-            crono1.Stop();
             crono2.Start();
             Thread.Sleep(250);
+            crono1.Stop();
             crono2.Stop();
+
+            TimeSpan cts1 = crono1.Elapsed;
+            TimeSpan cts2 = crono2.Elapsed;
+
+            string elapsedTime1 = String.Format("{3.0000}", cts1.Seconds/1000);
+            string elapsedTime2 = String.Format("{3.0000}", cts2.Seconds/1000);
         }
     }
 }
