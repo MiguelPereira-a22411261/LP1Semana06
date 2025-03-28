3c3926692e0a20d82ee881522cef94118d8e225d
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-03-28 09:03:36 +0000

    Update first and second cicle

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -10,13 +10,13 @@ namespace MyGame
             Enemy[] enemies = new Enemy[nEnemy];
             for (int i = 0; i < nEnemy; i++)
             {
-                Console.Write($"Enter the name of enemy {i + 1}: ");
+                Console.Write($"Nome do inimigo {i + 1}: ");
                 string enemyName = Console.ReadLine();
                 enemies[i] = new Enemy(enemyName);
             }
             for (int i = 0; i < nEnemy; i++)
             {
-                Console.WriteLine($"Enemy {i + 1}: {enemies[i].GetName()}, {enemies[i].GetHealth()}, {enemies[i].GetShield()}");
+                Console.WriteLine($"{enemies[i].GetName()} + {enemies[i].GetHealth()} + {enemies[i].GetShield()}");
             }
         }
     }
