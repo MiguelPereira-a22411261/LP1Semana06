a243cd5501992430372de86485e7af251b1aaff5
MiguelPereira-a22411261 <miguel.pereirawork08@gmail.com>
2025-03-28 16:46:44 +0000

    Add base for MyRoguelike

---
+++ MyRoguelike/Enemy.cs
@@ -0,0 +1,74 @@
+using System;
+
+namespace MyGame
+{
+    public class Enemy
+    {
+        private string name;
+        private float health;
+        private float shield;
+        static int pwrupCount;
+        
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public static int GetPowerUpCount()
+        {
+            return pwrupCount;
+        }
+        public string SetName()
+        {
+            string scndname = Console.ReadLine();
+            name = scndname;
+            return name;
+
+        }
+        
+        public void PickupPowerUp(PowerUp powerUp, float powerVal)
+        {
+            if (powerUp == PowerUp.Health)
+            {
+                health += powerVal;
+                if (health > 100) health = 100;
+                Console.WriteLine("Health: " + health);
+            }
+            else if (powerUp == PowerUp.Shield)
+            {
+                shield += powerVal;
+                if (shield > 100) shield = 100;
+                Console.WriteLine("Shield: " + shield);
+            }
+            pwrupCount++;
+        }
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+    }
+}
---
+++ MyRoguelike/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int nEnemy = int.Parse(args[0]);
+            Enemy[] enemies = new Enemy[nEnemy];
+            for (int i = 0; i < nEnemy; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string enemyName = Console.ReadLine();
+                enemies[i] = new Enemy(enemyName);
+                enemies[i].TakeDamage(10);
+            }
+            for (int i = 0; i < nEnemy; i++)
+            {
+                if (enemies[i].GetName().Length > 8)
+                {
+                    enemies[i] = new Enemy(enemies[i].GetName().Substring(0, 8));
+                }
+                Console.WriteLine($"{enemies[i].GetName()} {enemies[i].GetHealth()} {enemies[i].GetShield()}");
+                enemies[i].PickupPowerUp(PowerUp.Health, 20);
+                enemies[i].PickupPowerUp(PowerUp.Shield, 20);
+                Console.WriteLine($"{enemies[i].GetName()} {enemies[i].GetHealth()} {enemies[i].GetShield()}");
+                Console.WriteLine(Enemy.GetPowerUpCount());
+            }
+        }
+    }
+}
